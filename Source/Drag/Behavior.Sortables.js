f8048338718c577adcfb1390126eb157867cab64
Aaron Newton <git@clientcide.com>
2010-10-06 22:23:09 -0700

    adding filter for sortables

---
+++ Source/Drag/Behavior.Sortables.js
@@ -0,0 +1,65 @@
+/*
+---
+description: Creates instances of Sortable for sortable lists, optionally updating an input or element with the sort state.
+provides: [Behavior.Sortables]
+requires: [Behavior/Behavior, More/Sortables, More/Scroller]
+script: Behavior.HtmlTable.js
+...
+*/
+(function(){
+
+Behavior.addGlobalFilters({
+
+	Sortable: function(element, methods){
+		var target = element.getData('sort-state');
+		if (target) target = element.getParent().getElement(target);
+		
+		var scrollParent;
+		var sort = new Sortables(element, {
+			clone: true,
+			opacity: 0.6,
+			onStart: function(){
+				var scroller,
+				    scrollElement = isScrollable(element) ? element : getScrollParent(element);
+				if (scrollElement && scrollElement != scrollParent) {
+					scroller = new Scroller(scrollElement);
+					scrollElement.store('behavior:scroller', scroller);
+					scrollParent = scrollElement;
+				} else {
+					if (scrollParent) scroller = scrollParent.retrieve('behavior:scroller');
+				}
+				if (scroller) scroller.attach();
+			},
+			onComplete: function(){
+				if (target) {
+					target.set(target.get('tag') == 'input' ? 'value' : 'html', sort.serialize());
+				}
+				if (scrollParent) scrollParent.retrieve('behavior:scroller').detach();
+			}
+		});
+	}
+
+});
+
+var isBody = function(element){
+	return (/^(?:body|html)$/i).test(element.tagName);
+};
+
+var isScrollable = function(element){
+	return ['scroll', 'auto'].contains(element.getStyle('overflow'));
+};
+
+var getScrollParent = function(element){
+	var scrollParent,
+	    parent = element.getParent();
+	while (!scrollParent){
+		if (isBody(parent) || isScrollable(parent)){
+			scrollParent = parent;
+		} else {
+			parent = parent.getParent();
+		}
+	}
+	return scrollParent;
+};
+
+})();
