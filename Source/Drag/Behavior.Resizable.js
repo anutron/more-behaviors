fa7aea4bd88a9357c7a636f5137abc6ea2956f38
Aaron Newton <git@clientcide.com>
2010-10-10 23:18:38 -0400

    adding resizable filter

---
+++ Source/Drag/Behavior.Resizable.js
@@ -0,0 +1,27 @@
+/*
+---
+description: Creates instances of Drag for resizable elements.
+provides: [Behavior.Resizable]
+requires: [Behavior/Behavior, More/Drag]
+script: Behavior.Resizable.js
+...
+*/
+(function(){
+
+	Behavior.addGlobalFilters({
+
+		Resizable: function(element, methods){
+			var options = {};
+			if (element.getData('resize-handle')) options.handle = element.getElement(element.getData('resize-handle'));
+			if (element.getData('resize-modifiers')) options.modifiers = element.getJSONData('resize-modifiers');
+			var target = element;
+			if (element.getData('resize-child')) target = element.getElement(element.getData('resize-child'));
+			var drag = target.makeResizable(options);
+			this.markForCleanup(element, function(){
+				drag.detach();
+			});
+		}
+
+	});
+
+})();
