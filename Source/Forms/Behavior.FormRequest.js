bcdce94974707a3282c69c2ca53b4a919229472b
Aaron Newton <git@clientcide.com>
2011-01-13 11:48:19 -0800

    FormRequest filter now uses getContentElement

--- Source/Forms/Behavior.FormRequest.js
+++ Source/Forms/Behavior.FormRequest.js
@@ -9,7 +9,7 @@ script: Behavior.FormRequest.js
 
 Behavior.addGlobalFilters({
 
-	FormRequest: function(element){
+	FormRequest: function(element, behaviorAPI){
 		var updateElement,
 		    update = element.getData('update');
 		if (update == "parent") {
@@ -19,11 +19,12 @@ Behavior.addGlobalFilters({
 		} else if (element.getData('update-by-id')){
 			updateElement = document.id(element.getData('update-by-id'));
 		} else if (element.getData('update-by-selector')){
-			updateElement = document.id(document.body).getElement(element.getData('update-by-selector'));
+			updateElement = document.id(behaviorAPI.getContentElement()).getElement(element.getData('update-by-selector'));
 		}
 		//pass null for the update element argument; JFrame does our updating for us
-		var req = new Form.Request(element, updateElement || element, {
+		var req = new Form.Request(element, updateElement || behaviorAPI.getContentElement(), {
 			requestOptions: {
+				filter: element.getData('update-filter'),
 				spinnerTarget: updateElement
 			},
 			resetForm: !element.hasClass('noReset')
